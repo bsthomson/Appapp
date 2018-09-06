89c6f14c320bbf29a5b2a21e598bb574553dbf6c
bsthomson <thomson.bs@gmail.com>
2018-09-06 18:56:33 -0500

    changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -34,52 +34,52 @@ module.exports = function(app) {
         res.json(dbFavorite);
       });
   });
-    app.get("/api/quiz", function(req, res) {
-        res.json(quiz);
-    });
+  app.get('/api/quiz', function(req, res) {
+    res.json(quiz);
+  });
 
-    app.post("/api/quiz", function(req, res) {
+  app.post('/api/quiz', function(req, res) {
         
         
-        var petName = "";
-        var petPic = "";
-        var petDesc = "";
+    var petName = '';
+    var petPic = '';
+    var petDesc = '';
 
-        var totalDifference = 1000;
+    var totalDifference = 1000;
 
-        quiz.forEach(function(results) {
+    quiz.forEach(function(results) {
 
-          console.log(results);
+      console.log(results);
 
-            var scoresArray = [];
-            var currentDifference = 1000;
+      var scoresArray = [];
+      var currentDifference = 1000;
 
-            function add(total, num) {
-                return total + num;
-            }
+      function add(total, num) {
+        return total + num;
+      }
 
-            for (var i = 0; i < results.scores.length; i++) {
-                scoresArray.push(Math.abs(parseInt(req.body.scores[i]) - parseInt(results.scores[i])));
+      for (var i = 0; i < results.scores.length; i++) {
+        scoresArray.push(Math.abs(parseInt(req.body.scores[i]) - parseInt(results.scores[i])));
 
-            }
+      }
 
-            currentDifference = scoresArray.reduce(add, 0);
-            console.log(currentDifference);
+      currentDifference = scoresArray.reduce(add, 0);
+      console.log(currentDifference);
             
-            if (currentDifference < totalDifference) {
-                totalDifference = currentDifference;
+      if (currentDifference < totalDifference) {
+        totalDifference = currentDifference;
 
-                petName = results.name;
-                petPic = results.pic;
-                petDesc = results.desc;
-            }
-        });
+        petName = results.name;
+        petPic = results.pic;
+        petDesc = results.desc;
+      }
+    });
 
         
 
-        res.json({ status: "ok", petName: petName, petPic: petPic, petDesc: petDesc });
+    res.json({ status: 'ok', petName: petName, petPic: petPic, petDesc: petDesc });
 
-        quiz.push(req.body);
-    });
+    quiz.push(req.body);
+  });
 };
 
